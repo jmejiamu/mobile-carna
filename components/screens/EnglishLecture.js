044da13c90197d2148700577a39744bf4f429078
Jose <jmejiamu@gmail.com>
2021-05-11 14:35:09 -0700

    added english list

--- components/screens/EnglishLecture.js
+++ components/screens/EnglishLecture.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { StyleSheet, Text, View, FlatList, Image, TouchableOpacity } from 'react-native'
 import restapi from '../url/url';
 
 const EnglishLecture = () => {
@@ -27,18 +27,92 @@ const EnglishLecture = () => {
 
             renderItem={({ item }) => {
                 return (
-                    <View style={{ flex: 1 }}>
-                        <View style={styles.card}>
-                            <Text style={{ marginLeft: 15 }}>title: {item.title} </Text>
-
+                    <TouchableOpacity style={styles.card} >
+                        <Image style={styles.image} source={{ uri: 'https://img.icons8.com/clouds/100/000000/groups.png' }} />
+                        <View style={styles.cardContent}>
+                            <Text style={styles.name}>{item.title}</Text>
+                            {/* <Text style={styles.count}>{item.content}</Text> */}
+                            <TouchableOpacity style={styles.followButton}>
+                                <Text style={styles.followButtonText}>Explore now</Text>
+                            </TouchableOpacity>
                         </View>
-                    </View>
+                    </TouchableOpacity>
                 )
             }}
         />
     )
 }
 
-export default EnglishLecture
+export default EnglishLecture;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 20,
+        backgroundColor: "#ebf0f7"
+    },
+    contentList: {
+        flex: 1,
+    },
+    cardContent: {
+        marginLeft: 20,
+        marginTop: 10
+    },
+    image: {
+        width: 90,
+        height: 90,
+        borderRadius: 45,
+        borderWidth: 2,
+        borderColor: "#ebf0f7"
+    },
+
+    card: {
+        shadowColor: '#00000021',
+        shadowOffset: {
+            width: 0,
+            height: 6,
+        },
+        shadowOpacity: 0.37,
+        shadowRadius: 7.49,
+        elevation: 12,
+
+        marginLeft: 20,
+        marginRight: 20,
+        marginTop: 20,
+        backgroundColor: "white",
+        padding: 10,
+        flexDirection: 'row',
+        borderRadius: 30,
+    },
+
+    name: {
+        fontSize: 18,
+        flex: 1,
+        alignSelf: 'center',
+        color: "#3399ff",
+        fontWeight: 'bold'
+    },
+    count: {
+        fontSize: 14,
+        flex: 1,
+        alignSelf: 'center',
+        color: "#6666ff"
+    },
+    followButton: {
+        marginTop: 10,
+        height: 35,
+        width: 100,
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 30,
+        backgroundColor: "white",
+        borderWidth: 1,
+        borderColor: "#dcdcdc",
+    },
+    followButtonText: {
+        color: "#dcdcdc",
+        fontSize: 12,
+    },
+})
